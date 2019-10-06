76fd843cb6fdc0c863676c8e89fc7da4b66410d5
Uzias Rivera <uziasrivera@gmail.com>
2019-10-06 15:39:16 -0700

    Updates

---
+++ precache-manifest.81641489e7665e11a57b97d718a45cb5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "352c29625316c57874dc29bbd428d533",
+    "url": "/job-app/index.html"
+  },
+  {
+    "revision": "4053de83e087260a6b24",
+    "url": "/job-app/static/css/2.0624914a.chunk.css"
+  },
+  {
+    "revision": "fb072fcce7650371a4b4",
+    "url": "/job-app/static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "4053de83e087260a6b24",
+    "url": "/job-app/static/js/2.6c644c37.chunk.js"
+  },
+  {
+    "revision": "fb072fcce7650371a4b4",
+    "url": "/job-app/static/js/main.07cde7ec.chunk.js"
+  },
+  {
+    "revision": "a5058af76a156114e184",
+    "url": "/job-app/static/js/runtime-main.950d2779.js"
+  }
+]);
