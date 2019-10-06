3468ee4c18e4031c8875ddb58207befc181c402c
Uzias Rivera <uziasrivera@gmail.com>
2019-10-06 16:20:52 -0700

    fixed things part 4

--- src/App.js
+++ src/App.js
@@ -3,18 +3,18 @@ import "./App.css";
 import JobGrid from './components/JobGrid'
 import JobJumbo from './components/JobJumbo'
 import styled from 'styled-components'
-import {Route, BrowserRouter as Router} from 'react-router-dom'
+import {HashRouter, Route, BrowserRouter as Router} from 'react-router-dom'
 import Job from './components/Job'
 
 
 function App() {
   return (
     <div className="App">
-    <Router>
+    <HashRouter>
       <Route exact path='/job-app' component={JobJumbo}/>
       <Route exact path='/job-app' component={JobGrid}/> 
       <Route path='/Job/:id' component={Job}/>
-    </Router>
+    </HashRouter>
     </div>
   );
 }
